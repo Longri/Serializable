bb9d4050c12443909d6c55836033d07977d0ecaa
Longri <www.longri.de@googlemail.com>
2015-11-05 11:55:40 +0100

    + remove not more used classes from "mapsforge" and thier tests

--- src/test/java/de/longri/serializable/SerializableTest.java
+++ src/test/java/de/longri/serializable/SerializableTest.java
@@ -28,38 +28,6 @@ public class SerializableTest {
         assertTrue(writer.getArray().length == 8);
     }
 
-    @Test
-    public void testVariableSignedIntegerObject() throws Exception {
-
-        TestObjectVariableByteSignedInteger obj = new TestObjectVariableByteSignedInteger();
-        obj.IntegerValue1 = 16310;
-        obj.IntegerValue2 = 3242341;
-
-        TestObjectVariableByteSignedInteger obj2 = new TestObjectVariableByteSignedInteger();
-
-        NormalStore writer = new NormalStore();
-        obj.serialize(writer);
-        obj2.deserialize(new NormalStore(writer.getArray()));
-
-        assertEquals(obj, obj2);
-
-    }
-
-    @Test
-    public void testVariableUnignedIntegerObject() throws Exception {
-
-        TestObjectVariableByteUnsignedInteger obj = new TestObjectVariableByteUnsignedInteger();
-        obj.IntegerValue1 = 16310;
-        obj.IntegerValue2 = 3242341;
-
-        TestObjectVariableByteUnsignedInteger obj2 = new TestObjectVariableByteUnsignedInteger();
-
-        NormalStore writer = new NormalStore();
-        obj.serialize(writer);
-        obj2.deserialize(new NormalStore(writer.getArray()));
-
-        assertEquals(obj, obj2);
-    }
 
     @Test
     public void testObject() throws Exception {
