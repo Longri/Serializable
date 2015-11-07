28c131b02cd94682e9fb8fdc6ef7365eb52b8f76
Longri <www.longri.de@googlemail.com>
2015-11-07 12:06:13 +0100

    + remove unused masks

--- src/main/java/de/longri/serializable/BitStore.java
+++ src/main/java/de/longri/serializable/BitStore.java
@@ -19,8 +19,6 @@ public class BitStore extends StoreBase {
     private final static int STATE_BIT_MASK_BYTE = 7;
     private final static int STATE_BIT_MASK_SHORT = 15;
 
-    private short SHORT_MASK;
-    private int INT_MASK;
 
     private class Pointer {
         int _Byte = 0;
@@ -70,29 +68,17 @@ public class BitStore extends StoreBase {
         //write bit's
         twoBytes = (short) (twoBytes | (short) b & MASK_8bit);
 
-
         //get two Bytes from buffer and put thier into a Short
         short buffervalue = (short) (((getBufferByte(pointer._Byte) & MASK_8bit) << 8) | (getBufferByte(pointer._Byte + 1) & MASK_8bit));
-
-
-        //SHORT_MASK for 3bits state & count
-        SHORT_MASK = 1;
-        for (int i = 1; i < count + STATE_BIT_COUNT_BYTE; i++) SHORT_MASK = (short) ((SHORT_MASK << 1) + 1);
 
         //shift the twoBytes to the right pointer
         twoBytes = (short) ((short) (twoBytes << ((16 - (STATE_BIT_COUNT_BYTE + count)) - pointer._Bit)));
 
-        //add twoBytes with Buffer
-        SHORT_MASK = 1;
-        for (int i = 1; i < 16 - pointer._Bit; i++) SHORT_MASK = (short) ((SHORT_MASK << 1) + 1);
-
-
-        twoBytes = (short) (twoBytes & SHORT_MASK | buffervalue);
+        twoBytes = (short) (twoBytes | buffervalue);
 
         //write back to Buffer
         setBufferByte(pointer._Byte, (byte) (twoBytes >> 8));
         setBufferByte(pointer._Byte + 1, (byte) (twoBytes));
-
 
         //move Pointer
         movePointer(count + STATE_BIT_COUNT_BYTE);
@@ -121,23 +107,13 @@ public class BitStore extends StoreBase {
         fourBytes = fourBytes | s & MASK_16bit;
 
         //get for Bytes from buffer and put thier into a Short
-        int buffervalue = getBufferByte(pointer._Byte) << 24 | (getBufferByte(pointer._Byte + 1) & 0xff) << 16
+        int bufferValue = getBufferByte(pointer._Byte) << 24 | (getBufferByte(pointer._Byte + 1) & 0xff) << 16
                 | (getBufferByte(pointer._Byte + 2) & 0xff) << 8 | (getBufferByte(pointer._Byte + 3) & 0xff);
-
-
-        //SHORT_MASK for 4bits state & count
-        INT_MASK = 1;
-        for (int i = 1; i < count + STATE_BIT_COUNT_SHORT; i++) INT_MASK = ((INT_MASK << 1) + 1);
 
         //shift the twoBytes to the right pointer
         fourBytes = ((fourBytes << ((32 - (STATE_BIT_COUNT_SHORT + count)) - pointer._Bit)));
 
-        //add twoBytes with Buffer
-        INT_MASK = 1;
-        for (int i = 1; i < 32 - pointer._Bit; i++) INT_MASK = ((INT_MASK << 1) + 1);
-
-
-        fourBytes = ((fourBytes & INT_MASK) | buffervalue);
+        fourBytes = (fourBytes | bufferValue);
 
         //write back to Buffer
         setBufferByte(pointer._Byte, (byte) (fourBytes >> 24));
@@ -168,68 +144,60 @@ public class BitStore extends StoreBase {
     public byte readByte() throws NotImplementedException {
 
         //copy two bytes from Buffer
-        short buffervalue = (short) (((getBufferByte(pointer._Byte) & MASK_8bit) << 8) | (getBufferByte(pointer._Byte + 1) & MASK_8bit));
+        short bufferValue = (short) (((getBufferByte(pointer._Byte) & MASK_8bit) << 8) | (getBufferByte(pointer._Byte + 1) & MASK_8bit));
 
         //shift left for pointer
-        buffervalue = (short) (buffervalue << pointer._Bit);
+        bufferValue = (short) (bufferValue << pointer._Bit);
 
         //read first three bits for count
-        int count = ((byte) (buffervalue >> 13) & STATE_BIT_MASK_BYTE);
+        int count = ((byte) (bufferValue >> 13) & STATE_BIT_MASK_BYTE);
 
         if (count == 0) count = 8;
 
         //shift out three count bits
-        buffervalue = (short) (buffervalue << STATE_BIT_COUNT_BYTE);
+        bufferValue = (short) (bufferValue << STATE_BIT_COUNT_BYTE);
 
         //create Mask
         int mask = 1;
         for (int i = 1; i < count; i++) mask = (mask << 1) + 1;
 
         //shift right to the first pos
-        buffervalue = (short) (buffervalue >> 8 + (8 - count) & mask);
-
-        byte ret = (byte) buffervalue;
+        bufferValue = (short) (bufferValue >> 8 + (8 - count) & mask);
 
         //move pointer
         movePointer(count + STATE_BIT_COUNT_BYTE);
 
-        return ret;
-
+        return (byte) bufferValue;
     }
 
     @Override
     public short readShort() throws NotImplementedException {
         //copy four bytes from Buffer
-        int buffervalue = getBufferByte(pointer._Byte) << 24 | (getBufferByte(pointer._Byte + 1) & 0xff) << 16
+        int bufferValue = getBufferByte(pointer._Byte) << 24 | (getBufferByte(pointer._Byte + 1) & 0xff) << 16
                 | (getBufferByte(pointer._Byte + 2) & 0xff) << 8 | (getBufferByte(pointer._Byte + 3) & 0xff);
 
-
         //shift left for pointer
-        buffervalue = (buffervalue << pointer._Bit);
-
+        bufferValue = (bufferValue << pointer._Bit);
 
         //read four bits for count
-        int count = ((byte) (buffervalue >> 28) & STATE_BIT_MASK_SHORT);
+        int count = ((byte) (bufferValue >> 28) & STATE_BIT_MASK_SHORT);
 
         if (count == 0) count = 16;
 
         //shift out three count bits
-        buffervalue = (buffervalue << STATE_BIT_COUNT_SHORT);
+        bufferValue = (bufferValue << STATE_BIT_COUNT_SHORT);
 
         //create Mask
         int mask = 1;
         for (int i = 1; i < count; i++) mask = (mask << 1) + 1;
 
         //shift right to the first pos
-        buffervalue = (buffervalue >> 32 + (32 - count) & mask);
-
-        short ret = (short) buffervalue;
+        bufferValue = (bufferValue >> 32 + (32 - count) & mask);
 
         //move pointer
         movePointer(count + STATE_BIT_COUNT_SHORT);
 
-        return ret;
-
+        return (short) bufferValue;
     }
 
     private byte getBufferByte(int byteIndex) {
