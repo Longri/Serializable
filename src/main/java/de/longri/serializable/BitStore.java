d32c0778156800b67a603640399e776be9870142
Longri <www.longri.de@googlemail.com>
2015-11-15 10:42:11 +0100

    + remove redundant code

--- src/main/java/de/longri/serializable/BitStore.java
+++ src/main/java/de/longri/serializable/BitStore.java
@@ -1,7 +1,6 @@
 package de.longri.serializable;
 
 import java.util.ArrayList;
-import java.util.BitSet;
 
 
 /**
@@ -9,22 +8,6 @@ import java.util.BitSet;
  */
 public class BitStore extends StoreBase {
 
-
-    private final static int MASK_8bit = 0xFF;
-    private final static int MASK_16bit = 0xFFFF;
-    private final static long MASK_32bit = 0xFFFFFFFF;
-    private final static long MASK_64bit = 0xFFFFFFFFFFFFFFFFL;
-
-    private final static int STATE_BIT_COUNT_BYTE = 3;
-    private final static int STATE_BIT_COUNT_SHORT = 4;
-    private final static int STATE_BIT_COUNT_INTEGER = 5;
-    private final static int STATE_BIT_COUNT_LONG = 6;
-
-    private final static int STATE_BIT_MASK_BYTE = 7;
-    private final static int STATE_BIT_MASK_SHORT = 15;
-    private final static int STATE_BIT_MASK_INTEGER = 31;
-
-    
     final static Number LONG = new Number(6, 64, Long.MIN_VALUE);
     final static Number INTEGER = new Number(5, 32, Integer.MIN_VALUE);
     final static Number SHORT = new Number(4, 16, Short.MIN_VALUE);
@@ -79,176 +62,42 @@ public class BitStore extends StoreBase {
 
     @Override
     protected void _write(byte b) throws NotImplementedException {
-
-        // write one bit vor negative/positive value
+        boolean negative = false;
         if (b < 0) {
-            write(true);
-            b = (byte) -b;
-        } else write(false);
-
-
-        short twoBytes = 0;
-        int count = 0;
-
-        if (b < 0) {
-            count = 8;
-        } else if (b == 0) {
-            count = 1;
-        } else {
-            //get index of first HIGH bit
-            BitSet set = BitSet.valueOf(new byte[]{b});
-            int index = set.previousSetBit(7);
-
-            //calc bit count
-            count = index + 1;
+            negative = true;
+            if (b == Short.MIN_VALUE) b = 0;
+            else
+                b = (byte) -b;
         }
-
-
-        //write count bits
-        twoBytes = (short) (count & STATE_BIT_MASK_BYTE);
-
-        //shift to have place for bit's
-        twoBytes = (short) (twoBytes << count);
-
-        //write bit's
-        twoBytes = (short) (twoBytes | ((short) (b & MASK_8bit)));
-
-        //get two Bytes from buffer and put they into a Short
-        short bufferValue = (short) (((getBufferByte(pointer._Byte) & MASK_8bit) << 8) | (getBufferByte(pointer._Byte + 1) & MASK_8bit));
-
-        //shift the twoBytes to the right pointer
-        twoBytes = (short) ((short) (twoBytes << ((16 - (STATE_BIT_COUNT_BYTE + count)) - pointer._Bit)));
-
-        twoBytes = (short) (twoBytes | bufferValue);
-
-        //write back to Buffer
-        setBufferByte(pointer._Byte, (byte) (twoBytes >> 8));
-        setBufferByte(pointer._Byte + 1, (byte) (twoBytes));
-
-        //move Pointer
-        movePointer(count + STATE_BIT_COUNT_BYTE + 1);
-
+        byte[] bytes = new byte[]{0, b};
+        writeValue(negative, bytes, BYTE);
     }
 
 
     @Override
     protected void _write(short s) throws NotImplementedException {
-
-        // write one bit vor negative/positive value
+        boolean negative = false;
         if (s < 0) {
-            write(true);
-            s = (short) -s;
-        } else write(false);
-
-        int fourBytes = 0;
-
-        short v = s;
-        int count = 1;
-        while (true) {
-            v = (short) (v >>> 1);
-            if (v == 0 | v == -1) break;
-            count++;
+            negative = true;
+            if (s == Short.MIN_VALUE) s = 0;
+            else
+                s = (short) -s;
         }
-
-        //write count bits
-        fourBytes = (int) count;
-
-        //shift to have place for bit's
-        fourBytes = fourBytes << count;
-
-        //write bit's
-        fourBytes = fourBytes | (s & MASK_16bit);
-
-        //get for Bytes from buffer and put they into a Short
-        int bufferValue = getBufferByte(pointer._Byte) << 24 | (getBufferByte(pointer._Byte + 1) & 0xff) << 16
-                | (getBufferByte(pointer._Byte + 2) & 0xff) << 8 | (getBufferByte(pointer._Byte + 3) & 0xff);
-
-        //shift the twoBytes to the right pointer
-        fourBytes = ((fourBytes << ((32 - (STATE_BIT_COUNT_SHORT + count)) - pointer._Bit)));
-
-        fourBytes = (fourBytes | bufferValue);
-
-        //write back to Buffer
-        setBufferByte(pointer._Byte, (byte) (fourBytes >> 24));
-        setBufferByte(pointer._Byte + 1, (byte) (fourBytes >> 16));
-        setBufferByte(pointer._Byte + 2, (byte) (fourBytes >> 8));
-        setBufferByte(pointer._Byte + 3, (byte) fourBytes);
-
-        //move Pointer
-        movePointer(count + STATE_BIT_COUNT_SHORT + 1);
+        byte[] bytes = new byte[]{0, (byte) (s >> 8), (byte) s};
+        writeValue(negative, bytes, SHORT);
     }
 
     @Override
     protected void _write(int i) throws NotImplementedException {
-
-        if (false) {
-            boolean negative = false;
-            if (i < 0) {
-                negative = true;
-                if (i == Integer.MIN_VALUE) i = 0;
-                else
-                    i = -i;
-            }
-            byte[] bytes = new byte[]{0, (byte) (i >> 24), (byte) (i >> 16), (byte) (i >> 8), (byte) i};
-            writeValue(negative, bytes, INTEGER);
-
-        } else {
-            // write one bit vor negative/positive value
-            if (i < 0) {
-                write(true);
-                if (i == Integer.MIN_VALUE) i = 0;
-                else
-                    i = -i;
-            } else write(false);
-
-            long eightBytes = 0;
-
-            int v = i;
-            int count = 1;
-            while (true) {
-                v = v >>> 1;
-                if (v == 0 | v == -1) break;
-                count++;
-            }
-
-            //write count bits
-            eightBytes = (long) count;
-
-            //shift to have place for bit's
-            eightBytes = eightBytes << count;
-
-            //write bit's
-            eightBytes = eightBytes | (i & MASK_32bit);
-
-            //get eight Bytes from buffer and put they into a Short
-            long bufferValue = (getBufferByte(pointer._Byte) & 0xffL) << 56
-                    | (getBufferByte(pointer._Byte + 1) & 0xffL) << 48
-                    | (getBufferByte(pointer._Byte + 2) & 0xffL) << 40
-                    | (getBufferByte(pointer._Byte + 3) & 0xffL) << 32
-                    | (getBufferByte(pointer._Byte + 4) & 0xffL) << 24
-                    | (getBufferByte(pointer._Byte + 5) & 0xffL) << 16
-                    | (getBufferByte(pointer._Byte + 6) & 0xffL) << 8
-                    | (getBufferByte(pointer._Byte + 7) & 0xffL);
-
-
-            //shift the eightBytes to the right pointer
-            eightBytes = ((eightBytes << ((64 - (STATE_BIT_COUNT_INTEGER + count)) - pointer._Bit)));
-
-            eightBytes = (eightBytes | bufferValue);
-
-            //write back to Buffer
-            setBufferByte(pointer._Byte, (byte) (eightBytes >> 56));
-            setBufferByte(pointer._Byte + 1, (byte) (eightBytes >> 48));
-            setBufferByte(pointer._Byte + 2, (byte) (eightBytes >> 40));
-            setBufferByte(pointer._Byte + 3, (byte) (eightBytes >> 32));
-            setBufferByte(pointer._Byte + 4, (byte) (eightBytes >> 24));
-            setBufferByte(pointer._Byte + 5, (byte) (eightBytes >> 16));
-            setBufferByte(pointer._Byte + 6, (byte) (eightBytes >> 8));
-            setBufferByte(pointer._Byte + 7, (byte) eightBytes);
-
-            //move Pointer
-            movePointer(count + STATE_BIT_COUNT_INTEGER + 1);
+        boolean negative = false;
+        if (i < 0) {
+            negative = true;
+            if (i == Integer.MIN_VALUE) i = 0;
+            else
+                i = -i;
         }
+        byte[] bytes = new byte[]{0, (byte) (i >> 24), (byte) (i >> 16), (byte) (i >> 8), (byte) i};
+        writeValue(negative, bytes, INTEGER);
     }
 
 
@@ -331,135 +180,17 @@ public class BitStore extends StoreBase {
 
     @Override
     public byte readByte() throws NotImplementedException {
-
-        // read if Negative value
-        boolean isNegative = readBool();
-
-        //copy two bytes from Buffer
-        short bufferValue = (short) (((getBufferByte(pointer._Byte) & MASK_8bit) << 8) | (getBufferByte(pointer._Byte + 1) & MASK_8bit));
-
-        //shift left for pointer
-        bufferValue = (short) (bufferValue << pointer._Bit);
-
-        //read first three bits for count
-        int count = ((byte) (bufferValue >>> 13) & STATE_BIT_MASK_BYTE);
-
-        if (count == 0) count = 8;
-
-        //shift out three count bits
-        bufferValue = (short) (bufferValue << STATE_BIT_COUNT_BYTE);
-
-        //create Mask
-        int mask = 1;
-        for (int i = 1; i < count; i++) mask = (mask << 1) + 1;
-
-        //shift right to the first pos
-        bufferValue = (short) (bufferValue >>> 8 + (8 - count) & mask);
-
-        //move pointer
-        movePointer(count + STATE_BIT_COUNT_BYTE + 1);
-
-        byte ret = (byte) bufferValue;
-
-        if (isNegative) {
-            if (ret == 0) ret = Byte.MIN_VALUE;
-            else
-                ret = (byte) -ret;
-        }
-
-        return ret;
+        return (byte) readValue(BYTE);
     }
 
     @Override
     public short readShort() throws NotImplementedException {
-
-        // read if Negative value
-        boolean isNegative = readBool();
-
-        //copy four bytes from Buffer
-        int bufferValue = getBufferByte(pointer._Byte) << 24
-                | (getBufferByte(pointer._Byte + 1) & 0xff) << 16
-                | (getBufferByte(pointer._Byte + 2) & 0xff) << 8
-                | (getBufferByte(pointer._Byte + 3) & 0xff);
-
-        //shift left for pointer
-        bufferValue = (bufferValue << pointer._Bit);
-
-        //read four bits for count
-        int count = (byte) ((bufferValue >>> 28) & STATE_BIT_MASK_SHORT);
-
-        if (count == 0) count = 16;
-
-        //shift out three count bits
-        bufferValue = (bufferValue << STATE_BIT_COUNT_SHORT);
-
-        //create Mask
-        int mask = 1;
-        for (int i = 1; i < count; i++) mask = (mask << 1) + 1;
-
-        //shift right to the first pos
-        bufferValue = (bufferValue >>> 32 + (32 - count) & mask);
-
-
-        //move pointer
-        movePointer(count + STATE_BIT_COUNT_SHORT + 1);
-
-        short ret = (short) bufferValue;
-
-        if (isNegative) {
-            if (ret == 0) ret = Short.MIN_VALUE;
-            else
-                ret = (short) -ret;
-        }
-
-        return ret;
+        return (short) readValue(SHORT);
     }
 
     @Override
     public int readInt() throws NotImplementedException {
-
-        // read if Negative value
-        boolean isNegative = readBool();
-
-        //copy eight bytes from Buffer
-        long bufferValue = (getBufferByte(pointer._Byte) & 0xffL) << 56
-                | (getBufferByte(pointer._Byte + 1) & 0xffL) << 48
-                | (getBufferByte(pointer._Byte + 2) & 0xffL) << 40
-                | (getBufferByte(pointer._Byte + 3) & 0xffL) << 32
-                | (getBufferByte(pointer._Byte + 4) & 0xffL) << 24
-                | (getBufferByte(pointer._Byte + 5) & 0xffL) << 16
-                | (getBufferByte(pointer._Byte + 6) & 0xffL) << 8
-                | (getBufferByte(pointer._Byte + 7) & 0xffL);
-
-        //shift left for pointer
-        bufferValue = (bufferValue << pointer._Bit);
-
-        //read four bits for count
-        int count = (byte) ((bufferValue >>> 59) & STATE_BIT_MASK_INTEGER);
-
-        if (count == 0) count = 32;
-
-        //shift out three count bits
-        bufferValue = (bufferValue << STATE_BIT_COUNT_INTEGER);
-
-        //create Mask
-        int mask = 1;
-        for (int i = 1; i < count; i++) mask = (mask << 1) + 1;
-
-        //shift right to the first pos
-        bufferValue = (bufferValue >> 64 + (64 - count) & mask);
-
-        //move pointer
-        movePointer(count + STATE_BIT_COUNT_INTEGER + 1);
-
-        int ret = (int) bufferValue;
-
-        if (isNegative) {
-            if (ret == 0) ret = Integer.MIN_VALUE;
-            else
-                ret = (int) -ret;
-        }
-        return ret;
+        return (int) readValue(INTEGER);
     }
 
     @Override
